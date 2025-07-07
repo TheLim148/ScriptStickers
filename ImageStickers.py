64b0f88399bbf5394eb76518c8566489ead80b52
TheLim148 <ruzhov34@gmail.com>
2025-07-07 12:46:43 +0300

    Added sort, deleting existing files, and solve problem with names of photos

--- ImageStickers.py
+++ ImageStickers.py
@@ -2,6 +2,11 @@ from PIL import Image
 import shutil
 import subprocess
 import os
+import re
+
+def extractNumber(filename):
+    match = re.search(r'\d+', filename)
+    return int(match.group()) if match else -1 
 
 def tempFiles(dir_to):
     bat_path = os.path.join(dir_to, "delete_all.bat")
@@ -13,21 +18,21 @@ def tempFiles(dir_to):
 
     subprocess.Popen([bat_path], creationflags=subprocess.CREATE_NO_WINDOW)
 
-def only_name(file_path):
+def onlyName(file_path):
     extension = "." + file_path.split('.')[-1]
     return file_path.replace(extension,"")
 
-def make_avatar(avatar_path, file):
-    rename(avatar_path)
+def makeAvatar(avatar_path, file):
+    rename(avatar_path, None, option=1)
     avatar = Image.open(f"{avatar_path}/{file}")
     if(avatar.width == avatar.height):
         avatar = avatar.convert("RGBA")
         avatar = avatar.resize((100, 100))
-        avatar = avatar.save(f"{avatar_path}/{only_name(file)}.png", optimize = True)
+        avatar = avatar.save(f"{avatar_path}/{onlyName(file)}.png", optimize = True)
     else:
         try:
             avatar1 = avatar.copy()
-            avatar1.save(f"{avatar_path}/{only_name(file)}_original.jpg")
+            avatar1.save(f"{avatar_path}/{onlyName(file)}_original.jpg")
             avatar.close()
             os.mkdir(f"{avatar_path}/temp1")
             os.mkdir(f"{avatar_path}/temp2")
@@ -35,28 +40,43 @@ def make_avatar(avatar_path, file):
             os.rename(f"{avatar_path}/{file}", f"{avatar_path}/temp1/{file}")
             dir_from = f"{avatar_path}/temp1"
             dir_to = f"{avatar_path}/temp2"
-            change(dir_from, dir_to)
-            os.rename(f"{avatar_path}/temp2/{only_name(file)}.png", f"{avatar_path}/{only_name(file)}.png")
+            changePhotos(dir_from, dir_to)
+            os.rename(f"{avatar_path}/temp2/{onlyName(file)}.png", f"{avatar_path}/{onlyName(file)}.png")
             
             shutil.rmtree(f"{avatar_path}/temp1")
             shutil.rmtree(f"{avatar_path}/temp2")
 
-            avatar = Image.open(f"{avatar_path}/{only_name(file)}.png")
+            avatar = Image.open(f"{avatar_path}/{onlyName(file)}.png")
             avatar = avatar.resize((100, 100))
-            avatar = avatar.save(f"{avatar_path}/{only_name(file)}.png", optimize = True)
+            avatar = avatar.save(f"{avatar_path}/{onlyName(file)}.png", optimize = True)
         except FileExistsError:
             pass
 
-def rename(input_dir):
-    files = sorted(os.listdir(input_dir))
-    for i, file in enumerate(files):
-        os.rename(f"{input_dir}/{file}", f"{input_dir}/temp_{i}.jpg")
+def rename(dir_from, dir_to, option):
+    match(option):
+        case 1:
+            files = sorted(os.listdir(dir_from), key=extractNumber)
+            for i, file in enumerate(files):
+                os.rename(f"{dir_from}/{file}", f"{dir_from}/temp_{i}.jpg")
 
-    temp_files = sorted(os.listdir(input_dir))
-    for i, file in enumerate(temp_files):
-        os.rename(f"{input_dir}/{file}", f"{input_dir}/{i}.jpg")
+            temp_files = sorted(os.listdir(dir_from), key=extractNumber)
+            for i, file in enumerate(temp_files):
+                os.rename(f"{dir_from}/{file}", f"{dir_from}/{i}.jpg")
+        
+        case 2:
+            listWithPhotoNames = []
+            filesDirTo = sorted(os.listdir(dir_to), key=extractNumber)
+            for i, file in enumerate(filesDirTo):
+                listWithPhotoNames.append(int(onlyName(file)))
+            
+            filesDirFrom = sorted(os.listdir(dir_from), key=extractNumber)
+            for i, file in enumerate(filesDirFrom, start = len(listWithPhotoNames)):
+                os.rename(f"{dir_from}/{file}", f"{dir_from}/{i}.jpg")
 
-def change_one_file(dir_from, file, dir_to):
+        case _:
+            pass
+                
+def changePhoto(dir_from, file, dir_to):
     image = Image.open(f"{dir_from}/{file}")
     canvas = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
     if (image.width <= image.height):
@@ -67,12 +87,11 @@ def change_one_file(dir_from, file, dir_to):
         coef = 512/image.width
         image = image.resize((round(image.width*coef), round(image.height*coef)))
         canvas.paste(image, (0, 256-round(image.height/2)))
-    
-    canvas = canvas.save(f"{dir_to}/{only_name(file)}.png", optimize = True)
 
-def change(dir_from, dir_to):
-    rename(dir_from)
-    files = sorted(os.listdir(dir_from))
+    canvas = canvas.save(f"{dir_to}/{onlyName(file)}.png", optimize = True)
+
+def changePhotos(dir_from, dir_to):
+    files = sorted(os.listdir(dir_from), key=extractNumber)
     for file in files:
         image = Image.open(f"{dir_from}/{file}")
         canvas = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
@@ -86,9 +105,9 @@ def change(dir_from, dir_to):
             image = image.resize((round(image.width*coef), round(image.height*coef)))
             canvas.paste(image, (0, 256-round(image.height/2)))
         
-        canvas = canvas.save(f"{dir_to}/{only_name(file)}.png", optimize = True)
+        canvas = canvas.save(f"{dir_to}/{onlyName(file)}.png", optimize = True)
 
-        if(os.path.getsize(f"{dir_to}/{only_name(file)}.png") >= 524_288):
+        if(os.path.getsize(f"{dir_to}/{onlyName(file)}.png") >= 524_288):
             temp_image = Image.open(f"{dir_from}/{file}")
             temp_image.save(f"{dir_from}/{file}", optimize = True, quality = 20)
-            change_one_file(dir_from, file, dir_to)
+            changePhoto(dir_from, file, dir_to)
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import ImageStickers as IS
+import os
+import shutil
+
+def clearDirectory(dir_path):
+    for item in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item)
+        
+        if os.path.isfile(item_path) or os.path.islink(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
 
 if __name__ == "__main__":
-    deleteFiles = False
     try:
-        deleteFiles = int(input("Do you want to delete your files after 30 minutes?" \
-                                "\n1 == True | 0 == False" \
+        deleteFiles = 0
+        deleteFiles = int(input("\nDo you want to delete your files after 30 minutes?" \
+                                "\n1 - Yes; 0 - No" \
                                 "\nP.S False by default" \
                                 "\n~$ "))
-        if (deleteFiles == 1):
-            deleteFiles = True
-        elif (deleteFiles == 0):
-            deleteFiles = False
-    except ValueError:
-        print("It's not a number!")
+    except Exception as e:
+        print(f"Error: {e}")
 
     while True:
-        choose = int(input("\n1 - сделать стикеры" \
+        choose = int(input( "\n1 - сделать стикеры" \
                             "\n2 - сделать аватар" \
                             "\n3 - переименовать файлы" \
                             "\n4 - выход" \
@@ -23,31 +30,68 @@ if __name__ == "__main__":
 
         match(choose):
             case 1:
-                dir_from = input("\nВведите название исходной папки: ")
-                dir_to = input("Введите название итоговой папки: ")
-                try:
-                    IS.change(dir_from, dir_to)
-                    if (deleteFiles == True):
-                        IS.tempFiles(dir_to)
-                        print("Стикеры успешно созданы!\nОни будут автоматически удалены через 30 минут")
-                    print("Стикеры успешно созданы!")
-                except FileNotFoundError:
-                    print("There is no folder or file")
+                option = int(input("\nВы инициализируете стикеры или добавляете их уже к существующим?" \
+                                "\n(1 - новые стикеры, 2 - добавление к старым)" \
+                                "\n~$ "))
+                match(option):
+                    case 1: # New stickers
+                        try:
+                            dir_from = input("\nВведите название исходной папки: ")
+                            dir_to = input("Введите название итоговой папки: ")
+
+                            if(not os.listdir(dir_to)):
+                                IS.rename(dir_from, dir_to, option)
+                                IS.changePhotos(dir_from, dir_to)
+                            else:
+                                clearDirectory(dir_to)
+                                IS.rename(dir_from, dir_to, option)
+                                IS.changePhotos(dir_from, dir_to)
+
+                            if (deleteFiles == 1):
+                                IS.tempFiles(dir_to)
+                                print("Стикеры успешно созданы!\nОни будут автоматически удалены через 30 минут")
+                            print("Стикеры успешно созданы!")
+                        except Exception as e:
+                            print(f"Error: {e}")
+                        break
+
+                    case 2: # Addition to old stickers
+                        try:
+                            dir_from = input("\nВведите название исходной папки: ")
+                            dir_to = input("Введите название итоговой папки: ")
+                        
+                            IS.rename(dir_from, dir_to, option)
+                            IS.changePhotos(dir_from, dir_to)
+                            if (deleteFiles == 1):
+                                IS.tempFiles(dir_to)
+                                print("Стикеры успешно добавлены!\nОни будут автоматически удалены через 30 минут")
+                            print("Стикеры успешно добавлены!")
+                        
+                        except Exception as e:
+                            print(f"Error: {e}")
+                        break
+
+                    case _:
+                        print("Bruh")
+                        break
+
             case 2:
                 avatar_path = input("\nВведите путь до директории с файлом-аватаром: ")
                 file = input("Введите имя файла-аватара: ")
                 try:
-                    IS.make_avatar(avatar_path, file)
-                    if (deleteFiles == True):
-                        IS.tempFiles(dir_to)
+                    IS.makeAvatar(avatar_path, file)
+                    if (deleteFiles == 1):
+                        IS.tempFiles(avatar_path)
                         print("Аватарка готова!\nОна будут автоматически удалены через 30 минут")
                     print("Аватарка готова!")
                 except FileNotFoundError:
                     print("There is no file or folder")
+
             case 3:
                 input_dir = input("Введите директорию, где хотите переименовать файлы: ")
-                IS.rename(input_dir)
+                IS.rename(input_dir, None, option=1)
                 print("Переименование прошло успешно")
+
             case 4:
                 print("Поки!")
                 break
